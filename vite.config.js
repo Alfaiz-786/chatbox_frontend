75fdd8d57540bea749e2cb64b27674cc825106dd
unknown <you@example.com>
2024-05-02 10:11:28 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://chatbox-backend-9att.onrender.com",
+      "/api": "http://localhost:5000/",
     },
   },
 });
